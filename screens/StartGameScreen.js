869dcf0e38b0e710900e3361d22d74e25f3d87f9
Ali Cangül <cangulali1@gmail.com>
2024-02-10 16:44:39 +0300

    first commit

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+const { View, TextInput } = require("react-native");
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+}
+
+export default StartGameScreen;
